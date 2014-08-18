03148106499b18fc628196b86aeadcff40b20144
ashrocket <ashley@raiteri.net>
2014-08-18 12:30:24 -0400

    Unique Queues for Application Seperation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,8 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  Sidekiq.default_worker_options = { :queue => :schedule_loader }
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  Sidekiq.default_worker_options = { :queue => :schedule_loader }
+
 end

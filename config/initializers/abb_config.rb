1c6f6effbc7bb2937998bb60ee9823c917a9a774
ashrocket <ashley@raiteri.net>
2014-08-27 21:04:20 +0200

    ABB COnfig added back in:

--- config/initializers/abb_config.rb
+++
@@ -1,8 +0,0 @@
-ABBConfig.all.each do |cfg|
-  puts "#{cfg.var}=#{cfg.value}"
-  ABBConfig.send("#{cfg.var}=",cfg.value)
-end
-ABBConfig.save_default(:data_key,'SNN')
-ABBConfig.save_default(:mct, 90)
-ABBConfig.save_default(:maxct, 1560)
-ABBConfig.save_default(:maxmaxct, 2880)

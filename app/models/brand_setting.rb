c0c520348de871ad557642f83a05fe8d08ca4474
ashrocket <ashley@raiteri.net>
2016-09-28 11:39:29 +0200

    Initializer for setting default currency on New Brand

--- app/models/brand_setting.rb
+++ app/models/brand_setting.rb
@@ -2,5 +2,10 @@ class BrandSetting < ActiveRecord::Base
   belongs_to :brand
   validates_presence_of :default_currency
   validates_presence_of :max_segments
+  after_initialize :defaults, unless: :persisted?
+  # ":if => :new_record?" is equivalent in this context
 
+  def defaults
+    self.default_currency ||= 'SGD'
+  end
 end

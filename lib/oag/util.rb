92fb12c9324871a7262fe31ccc1ea62074d8de22
ashrocket <ashley@raiteri.net>
2016-11-07 12:45:56 +0100

    Commented out Zip Utilities since they are outdated

--- lib/oag/util.rb
+++ lib/oag/util.rb
@@ -9,19 +9,19 @@ module Oag
   extend self
 
 
-
-    def uncompress_report archive_path, tmpfile_path
-      Zip::InputStream::open(archive_path) {|io|
-          entry = io.get_next_entry
-          File.open( tmpfile_path, 'wb'){|f| f << io.read}
-      }
-    end
-
-    def compress_report archive_path, tmpfile_path
-      Zip::File.open(archive_path, Zip::File::CREATE) do |zipfile|
-          zipfile.add(File.basename(tmpfile_path), tmpfile_path)
-      end
-    end
+    # Unused
+    # def uncompress_report archive_path, tmpfile_path
+    #   Zip::InputStream::open(archive_path) {|io|
+    #       entry = io.get_next_entry
+    #       File.open( tmpfile_path, 'wb'){|f| f << io.read}
+    #   }
+    # end
+    #
+    # def compress_report archive_path, tmpfile_path
+    #   Zip::File.open(archive_path, Zip::File::CREATE) do |zipfile|
+    #       zipfile.add(File.basename(tmpfile_path), tmpfile_path)
+    #   end
+    # end
 
 
   def release_forked_process_postgres_db_connection

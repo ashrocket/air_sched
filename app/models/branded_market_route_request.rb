84f1aeafcdd8b53250021ff8a41ca5ca53e41fc7
ashrocket <ashley@raiteri.net>
2016-11-07 07:58:46 +0100

    removed acts_as_list

--- app/models/branded_market_route_request.rb
+++ app/models/branded_market_route_request.rb
@@ -1,9 +1,15 @@
 class BrandedMarketRouteRequest < ActiveRecord::Base
   # attr_accessor :brand_id, , :origin, :dest, :cxrs, :host
 
+  belongs_to :brand
   belongs_to :branded_market_request
   belongs_to :branded_route_request
-  acts_as_list :scope => :branded_market_request
+
+
+  validates :brand, :branded_market_request, :branded_route_request, presence: true
+  validates_uniqueness_of :branded_market_request_id, scope: [:brand_id, :branded_route_request_id]
+
+  # acts_as_list :scope => :branded_market_request
 
 
 
